ed49034af4e65074eaf9c8daccd78a403f2272d6
gibsonbailey <gibson.bailey@live.com>
2019-03-13 17:48:27 -0700

    SUDO_USER does not work when running as root. Use your own username instead.

--- scripts/GCE_GPU_Startup.sh
+++ scripts/GCE_GPU_Startup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -x
 
-echo "SUDO_USER =" $SUDO_USER
+USER_NAME=your_username
 
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt -y update
 sudo apt-get -y install nvidia-384
 
-sudo -u $SUDO_USER curl https://get.docker.com | sudo sh
+sudo -u $USER_NAME curl https://get.docker.com | sudo sh
 sudo groupadd docker
-sudo usermod -aG docker $SUDO_USER
+sudo usermod -aG docker $USER_NAME
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-sudo -u $SUDO_USER newgrp docker <<EOF
+sudo -u $USER_NAME newgrp docker <<EOF
 
-sudo -u $SUDO_USER curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+sudo -u $USER_NAME curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
   sudo apt-key add -
-sudo -u $SUDO_USER curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+sudo -u $USER_NAME curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get -y update
 
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
 
-sudo -u $SUDO_USER docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
+sudo -u $USER_NAME docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
 EOF
 
